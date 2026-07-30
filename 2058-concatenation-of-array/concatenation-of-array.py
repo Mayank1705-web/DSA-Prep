02951b157ce50924c9b422d76436d8032cff2268
Mayank Ingole <mayankingole17may@gmail.com>
2026-07-30 23:50:41 +0530

    Time: 2 ms (24.02%) | Memory: 12.6 MB (69.84%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        ans = []
+        while(len(ans) != 2*len(nums)):
+            for i in range(len(nums)):
+                ans.append(nums[i])
+        return ans
+        
