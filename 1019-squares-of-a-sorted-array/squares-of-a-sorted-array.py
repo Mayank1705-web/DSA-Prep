1bf83311a408bc3441afdfd9f9c46d6be90b746a
Mayank Ingole <mayankingole17may@gmail.com>
2026-08-01 03:35:27 +0530

    Time: 3 ms (97.14%) | Memory: 14.3 MB (20.41%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return sorted(list(map(lambda x: x ** 2, nums)))
+        
