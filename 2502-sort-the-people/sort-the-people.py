29cebe28366e7c0111c43da4b9c7dc14ab2e7b8e
Mayank Ingole <mayankingole17may@gmail.com>
2026-08-07 18:16:44 +0530

    Time: 8 ms (56.41%) | Memory: 12.7 MB (9.92%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -7,7 +7,6 @@ class Solution(object):
         """
         result = []
         for person in sorted(zip(names, heights), key = lambda x: x[1], reverse = True):
-            name = person[0]
-            result.append(name)
+            result.append(person[0])
         
         return result
